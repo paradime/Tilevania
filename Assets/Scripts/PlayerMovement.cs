a79f85c74f7303b83ca14221ca3e467927809e80
paradime <brw4824@rit.edu>
2022-07-29 22:46:25 -0400

    mac push

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,27 +12,36 @@ public class PlayerMovement : MonoBehaviour
     private CapsuleCollider2D _collider;
 
     private static readonly string IS_RUNNING_ANIMATOR_FIELD = "IsRunning";
+    private static readonly string IS_CLIMBING_ANIMATOR_FIELD = "IsClimbing";
     private static readonly string GROUND_LAYER = "Ground";
+    private static readonly string LADDER_LAYER = "Ladder";
 
     [SerializeField]
     private float baseSpeed = 10f;
     [SerializeField]
+    private float climbSpeed = 7f;
+    [SerializeField]
     private float jumpSpeed = 5f;
+    
+    private float _defaultGravity;
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         _collider = GetComponent<CapsuleCollider2D>();
+        _defaultGravity = _rigidbody.gravityScale;
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        Climb();
     }
 
     private bool HasHorizontalVelocity() => Mathf.Abs(_rigidbody.velocity.x) > Mathf.Epsilon;
+    private bool HasVerticalVelocity() => Mathf.Abs(_rigidbody.velocity.y) > Mathf.Epsilon;
     private void FlipSprite()
     {
         if (!HasHorizontalVelocity())
@@ -45,7 +54,6 @@ public class PlayerMovement : MonoBehaviour
     void OnMove(InputValue value)
     {
         _moveInput = value.Get<Vector2>();
-        Debug.Log(_moveInput);
     }
 
     void Run()
@@ -54,6 +62,21 @@ public class PlayerMovement : MonoBehaviour
         _rigidbody.velocity = playerVelocity;
 
         _animator.SetBool(IS_RUNNING_ANIMATOR_FIELD, HasHorizontalVelocity());
+    }
+
+    void Climb()
+    {
+        if (!_collider.IsTouchingLayers(LayerMask.GetMask(LADDER_LAYER)))
+        {
+            _animator.SetBool(IS_CLIMBING_ANIMATOR_FIELD, false);
+            _rigidbody.gravityScale = _defaultGravity;
+            return;
+        }
+
+        _rigidbody.gravityScale = 0;
+        var playerVelocity = new Vector2(_rigidbody.velocity.x, _moveInput.y * climbSpeed);
+        _rigidbody.velocity = playerVelocity;
+        _animator.SetBool(IS_CLIMBING_ANIMATOR_FIELD, HasVerticalVelocity());
     }
 
     void OnJump(InputValue value)
